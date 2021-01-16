eab199bea4a1f13258054aaeb64a19380204fa5e
chyang0107 <chyang0107@gmail.com>
2021-01-16 12:41:53 -0600

    class 6

--- jquery.js
+++ jquery.js
@@ -1,2 +1,14 @@
 var inoutEl = document.querySelector('input')
 var buttonEl = document.querySelector('button')
+var ulEl = document.querySelector('ul')
+buttonEl.addEventListener('click', addTodo)
+
+function addTodo(){
+    var userInput = inputEl.value;
+    if (userInput.trim() == '') {
+        return;
+    }
+    var todoLi = document.createElement('LI');
+    todoLi.textContent = userInput;
+    ulEl.appendChild(todoLi);
+}
