941b3c66fa69e1a9ef42abcb14113ae8295c5922
chyang0107 <chyang0107@gmail.com>
2021-01-21 22:31:01 -0600

    change 1-13

--- jquery.js
+++ jquery.js
@@ -1,21 +1,34 @@
 var inputEl = document.querySelector('input')
 var buttonEl = document.querySelector('button')
 var ulEl = document.querySelector('ul')
+var todos = []
 buttonEl.addEventListener('click', addTodo)
+
 
 function addTodo(){
     var userInput = inputEl.value;
     if (userInput.trim() == '') {
         return;
     }
+    var newTodo = {id: Math.random(), value:userInput}
+    todos.push(newTodo)
     var todoLi = document.createElement('LI');
     todoLi.textContent = userInput;
     todoLi.addEventListener('click', removeTodo)
+    todoLi.dataset.id = newTodo.id
+
     ulEl.appendChild(todoLi);
 }
 
 function removeTodo(event) {
     var clickedLi = event.target;
     // removeChild should be invoked on the parent, i.e.:
+    var itemid = clickedLi.dataset.id
+    for (var i = 0; i<todos.length; i++){
+        if(todos[i].id ==itemid){
+            todos.splice(i,1);
+            break;
+        }
+    }
     clickedLi.parentNode.removeChild(clickedLi);
   }
