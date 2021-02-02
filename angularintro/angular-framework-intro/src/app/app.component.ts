31ba5c6f8f89d5b3a7608373d2c39fbaf1df5d19
chyang0107 <chyang0107@gmail.com>
2021-02-01 22:00:04 -0600

    angular practice

--- angularintro/angular-framework-intro/src/app/app.component.ts
+++ angularintro/angular-framework-intro/src/app/app.component.ts
@@ -7,7 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name = 'Mary';
+  elements: number[] = [];
+
   onChangeName(){
     this.name = 'Anna';
   }
+
+  onAddElement(){
+    this.elements.push(this.elements.length+1);
+  }
+
 }
