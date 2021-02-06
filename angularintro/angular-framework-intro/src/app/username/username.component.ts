df01420e0bb8d9b6a7f7106b40ea6f26462be3e7
chyang0107 <chyang0107@gmail.com>
2021-02-06 13:41:04 -0600

    angular practice

--- angularintro/angular-framework-intro/src/app/username/username.component.ts
+++ angularintro/angular-framework-intro/src/app/username/username.component.ts
@@ -1,15 +1,12 @@
-import { Component } from '@angular/core'
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-username',
-    template: 
-    `
+    template:`
         <p>{{username}}</p>
     `
-
 })
 
 export class UsernameComponent{
-    username = 'Jovis';
-
+  @Input() username ='Kevin';
 }
